28fa007621f19ce9e86c10f620396a0883b89294
MrNwear <mr.y.neuer@gmail.com>
2024-01-05 22:35:22 +0200

    input handled

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,35 +1,72 @@
 // Header.js
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Logo from "../logo";
-// const requestOptions = {
-//   method: "GET",
-//   headers: {
-//     Accept: "application/json, text/plain, */*",
-//     "Accept-Language": "ar-EG",
-//     // Add any other headers as needed
-//   },
-// };
+import { useDispatch } from "react-redux";
+import { fetchOrderDetails } from "../../redux/orderDetailsSlice/thunk";
+import "./style.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { isNumber } from "../../utils/helperFunctions";
+
 const Header = () => {
   const { t, i18n } = useTranslation();
-
+  const dispatch = useDispatch();
+  const [visible, setVisible] = useState(false);
+  const [shippingNumber, setShippingNumber] = useState({
+    value: "",
+    error: "",
+  });
+  const onInputChange = (e) => {
+    const inputValue = e.target.value;
+    if (isNumber(inputValue))
+      setShippingNumber({ value: inputValue, error: "" });
+    else {
+      setShippingNumber({ value: inputValue, error: t("header.input-error") });
+    }
+  };
+  const onTrackOrderPress = () => {
+    dispatch(fetchOrderDetails(shippingNumber.value)).then(() => {
+      setVisible(false);
+    });
+  };
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
 
   const isArabic = i18n.language === "ar";
   return (
-    <header style={headerStyles(isArabic)}>
+    <header className={`container ${isArabic ? "rtl" : "ltr"}`}>
       <Logo />
       <nav>
-        <ul style={navStyles}>
+        <ul className="navbar">
           <li>{t("header.tabs.home")}</li>
           <li>{t("header.tabs.pricing")}</li>
           <li>{t("header.tabs.call-center")}</li>
         </ul>
       </nav>
-      <div style={navStyles}>
-        <span>{t("header.tabs.track-order")}</span>
+      <div className="navbar">
+        <div>
+          <span
+            onClick={() => {
+              setVisible((visible) => !visible);
+            }}
+          >
+            {t("header.tabs.track-order")}
+          </span>
+          <div
+            class="inputWithButton"
+            style={{ display: visible ? "flex" : "none" }}
+          >
+            <input type="text" onChange={onInputChange} />
+            <AiOutlineSearch
+              className="icon-button"
+              onClick={onTrackOrderPress}
+            />
+            {shippingNumber.value && shippingNumber.error && (
+              <p className="error">{shippingNumber.error}</p>
+            )}
+          </div>
+        </div>
         <span>{t("header.tabs.login")}</span>
         <button onClick={() => changeLanguage(isArabic ? "en" : "ar")}>
           {t("header.switchLanguage")}
@@ -37,24 +74,6 @@ const Header = () => {
       </div>
     </header>
   );
-};
-
-const headerStyles = (isArabic) => ({
-  display: "flex",
-  alignItems: "center",
-  padding: "20px",
-  borderBottom: "1px solid #ccc",
-  justifyContent: "space-evenly",
-  direction: isArabic ? "rtl" : "ltr",
-});
-
-const navStyles = {
-  listStyle: "none",
-  padding: 0,
-  margin: 0,
-  display: "flex",
-  columnGap: 50,
-  marginLeft: "20px",
 };
 
 export default Header;
